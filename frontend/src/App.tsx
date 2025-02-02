3a5df2c221b1b86d74b85d5acddfd7656e40575d
arshitrupanii <arshitrupani6@gmail.com>
2025-02-02 17:54:14 +0530

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
-import './App.css'
+import { lazy, Suspense } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Loader from './components/Loader'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Home = lazy(() => import('./pages/Home'))
+const Search = lazy(() => import('./pages/Search'))
+const Cart = lazy(() => import('./pages/Cart'))
 
+const App = () => {
   return (
-    <>
-      <p className='text-7xl'>{count}</p>
-      <button onClick={()=> setCount(count+1)}>+</button>
-      <button onClick={()=> setCount(count-1)}>-</button>
-    </>
+    <BrowserRouter>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   )
 }
 
